7aa61d6056533d266ccce353eb7241ba71a6b21d
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-15 09:22:15 +0530

    udp client setup

--- SocketProgemmingUDP/src/UDP/UDPClient.java
+++ SocketProgemmingUDP/src/UDP/UDPClient.java
@@ -1,8 +1,20 @@
 package UDP;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class UDPClient {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		DatagramSocket client = new DatagramSocket();
+		InetAddress ipAdress = InetAddress.getLocalHost();
+		String str = "Hello world!";
+		byte[] buf = str.getBytes();
 		
+		DatagramPacket pac = new DatagramPacket(buf,buf.length,ipAdress,4222);
+		client.send(pac);
+		client.close();
 	}
 
 }
--- SocketProgemmingUDP/src/UDP/UDPServer.java
+++ SocketProgemmingUDP/src/UDP/UDPServer.java
@@ -1,8 +1,22 @@
 package UDP;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class UDPServer {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		DatagramSocket server = new DatagramSocket(4222);
+		byte[] buf = new byte[256];
 		
+		DatagramPacket packet = new DatagramPacket(buf,buf.length);
+		server.receive(packet);
+		
+		String message = new String(packet.getData());
+		System.out.println("Meddage is : " + message);
+		
+		server.close();
 	}
 
 }
